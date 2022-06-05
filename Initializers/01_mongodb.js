e3535a5024acec01f6089c66ba00367d0a504e82
Ben Bynum <ben@bbynum.com>
2022-06-04 22:03:41 -0500

    Add Users and mongodb mapping

---
+++ Initializers/01_mongodb.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+
+module.exports = gamedb = mongoose.createConnection(config.database);
---
+++ Models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {type: String, unique: true},
+    password: String,
+
+    sprite: String,
+
+    current_room: String,
+    pox_x: Number,
+    pos_y: Number
+});
+
+userSchema.statics.register = function(username, password, cb) {
+
+
+    let new_user = new User({
+        username: username,
+        password: password,
+
+        sprite: "spr_Hero",
+
+        current_room: maps[config.starting_zone].room,
+        pos_x: maps[config.starting_zone].start_x,
+        pos_y: maps[config.starting_zone].start_y
+    });
+
+    new_user.save(function (err) {
+        if (!err) {
+            cb(true);
+        } else {
+            cb(false);
+        }
+    });
+
+};
+
+userSchema.statics.login = function(username, password, cb) {
+    User.findOne({username: username}, function(err, user) {
+        if(!err && user) {
+            if (user.password == password) {
+                cb(true, user);
+            } else {
+                cb(false, null);
+            }
+        } else {
+            cb(false, null);
+        }
+    });
+};
+
+module.exports = User = gamedb.model('User', userSchema);
